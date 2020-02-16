d1e5cda2661ca7e5fffb12389e40b2e61cd6f2c7
BeCoolzz <sarthak.paneru007@gmail.com>
2020-02-16 21:43:11 +0545

    UI for todo homepage

--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -1,24 +1,68 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:Project_App/model/todo.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key key,
-   @required this.user
-  }) : super(key: key);
-  final AuthResult user;
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  final textController = TextEditingController();
+
+  List<TodoModel> tasks = [];
+
+  TodoModel currentTask;
+
   @override
   Widget build(BuildContext context) {
+    final TodoHelper _todoHelper = TodoHelper();
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home: ${widget.user.user.email}'),
+      appBar: AppBar(title: Text("TODO")),
+      body: Container(
+        padding:EdgeInsets.all(32),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: textController,
+            ),
+            FlatButton(
+              child: Text("Insert"),
+              onPressed: (){
+                currentTask = TodoModel(name: textController.text);
+                _todoHelper.insertTask(currentTask);
+              },
+              color: Colors.blue,
+              textColor: Colors.white,
+            ),
+            FlatButton(
+              child: Text("Show All Task"),
+              onPressed: () async{
+                List<TodoModel> list = await _todoHelper.getAllTask();
+
+                setState(() {
+                  tasks = list;
+                });
+              },
+              color: Colors.red,
+              textColor: Colors.white,
+            ),
+
+            Expanded(
+              child: ListView.separated(
+                  itemBuilder: (context, index){
+                    return ListTile(
+                      leading: Text("${tasks[index].id}"),
+                      title: Text("${tasks[index].name}"),
+                    );
+                  },
+                  separatorBuilder: (context, index) => Divider(),
+                  itemCount: tasks.length,
+
+              ),
+            )
+          ],
+        ),
       ),
-      body: Text('Hello!',),
     );
   }
 }
